90165d787125fe41c12703d35be00e131ae190dc
Geovanny Freire <geovannysilva888@gmail.com>
2026-09-22 23:48:11 -0300

    feat: adiciona tratamento de erros padronizado

---
+++ src/main/java/com/pactomais/bancoapi/exception/TratadorDeErros.java
@@ -0,0 +1,20 @@
+package com.pactomais.bancoapi.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class TratadorDeErros {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Map<String, String>> tratarErroGeral(RuntimeException ex) {
+        Map<String, String> erro = new HashMap<>();
+        erro.put("mensagem", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
+    }
+}
